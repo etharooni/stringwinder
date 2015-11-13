14ab3cef1ee17a97d51b2e69a073cfbc2dcf0751
Ethan Russell <erussell@pugetsound.edu>
2015-11-12 19:14:20 -0800

    added serial, fixed prescale issue

--- program.bat
+++ program.bat
@@ -1,3 +1,3 @@
-avr-g++ -g -Os -mmcu=atmega328 -c stringwinder.c
-avr-gcc -g -mmcu=atmega328 -o stringwinder.elf stringwinder.o
+avr-gcc -g -Os -mmcu=atmega328p -c stringwinder.cpp
+avr-gcc -g -mmcu=atmega328p -o stringwinder.elf stringwinder.o
 avr-objcopy -O ihex stringwinder.elf stringwinder.hex
